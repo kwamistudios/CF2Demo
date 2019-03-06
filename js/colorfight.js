6eb071fe15c136aa0bacd110070d467e461649e0
Jared Flores <jared_flores@cs.ucsb.edu>
2019-03-06 11:44:06 -0800

    Added WebSocket Query Created a handler for the WebSocket server to get the data. Still need to update the game map based on the values of the gold.

--- js/colorfight.js
+++ js/colorfight.js
@@ -16,6 +16,10 @@ var gameColumn = document.getElementById( "cf-game-col" ),
 	gameCanvas = document.getElementById( "cf-game-canvas" )
 	gameCtx = gameCanvas.getContext( "2d" );
 
+/* WEBSOCKET VARIABLES */
+var gameSocket = new WebSocket( "ws://colorfightii.herokuapp.com/game_channel" ),
+	gameData = false;
+
 /* Rendering the logo */
 function init_page() {
 	var logo = document.getElementById( "colorfight-logo" ),
@@ -41,15 +45,19 @@ function draw_game( ts ) {
 		cellWidth = gameWidth / 30 - 4;
 		cellHeight = gameWidth / 30 - 4;
 		cellRadius = 7;
-		gameCtx.fillStyle = "#FF0000";
+	}
+	gameCtx.clearRect( 0, 0, gameCanvas.width, gameCanvas.height );
+	if( gameData ) {
+		gameCtx.fillStyle = "#000000";
 		gameCtx.lineWidth = 2;
-		for( var x = 0; x < 30; x++ ) {
-			for( var y = 0; y < 30; y++ ) {
+		for( var y = 0; y < 30; y++ ) {
+			for( var x = 0; x < 30; x++ ) {
 				gameCtx.strokeStyle = "#65c9cf";
 				draw_cell( ( x * ( cellWidth + 4 ) ) + 1, ( y * ( cellHeight + 4 ) ) + 1, cellWidth, cellHeight, cellRadius );
 				gameCtx.stroke();
 				gameCtx.strokeStyle = "#faf334";
 				draw_cell( ( x * ( cellWidth + 4 ) ) + 3, ( y * ( cellHeight + 4 ) ) + 3, cellWidth - 4, cellHeight - 4, cellRadius - 2 );
+				gameCtx.fill();
 				gameCtx.stroke();
 			}
 		}
@@ -72,7 +80,21 @@ function draw_cell( x, y, cw, ch, cr ) {
 	gameCtx.closePath();
 }
 
+function hex_combine( src, dest, per ) {
+    var isrc = parseInt( src, 16 );
+    var idest = parseInt( dest, 16 );
+    var curr = Math.floor( isrc + ( idest - isrc ) * per );
+    return ( "0" + curr.toString( 16 ) ).slice( -2 ).toUpperCase();
+}
+
+function combine_color( src, dest, per ) {
+    if( per < 0 ) per = 0;
+    return "#" + hex_combine( src.slice( 1, 3 ), dest.slice( 1, 3 ), per ) + hex_combine( src.slice( 3, 5 ), dest.slice( 3, 5 ), per ) + hex_combine( src.slice( 5 ), dest.slice( 5 ), per );
+}
 
 init_page();
-
 window.requestAnimationFrame( draw_game );
+
+gameSocket.onmessage = function( msg ) {
+	gameData = JSON.parse( msg.data );
+}
